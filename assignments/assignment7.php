6186f0370ad8acedeeda17971b55ed7c6190f778
johann <112133694+jjrbf@users.noreply.github.com>
2023-03-08 21:16:37 -0800

    updated by devin

--- assignments/assignment7.php
+++ assignments/assignment7.php
@@ -15,71 +15,74 @@
     </form>
     
     <?php   
-            if (isset($_POST['search_txt'])) {
-                $search = $_POST['search_txt'];
-                if ($search === null)
-                    $search = "";
-                $CSVfp = fopen("menuForPHP.csv", "r");
-                if ($CSVfp !== FALSE) {
+        if (isset($_POST['search_txt'])) {
+            $search = $_POST['search_txt'];
+            if ($search === null)
+                $search = "";
+            $CSVfp = fopen("menuForPHP.csv", "r");
+            if ($CSVfp !== FALSE) {
 
-                    echo "<table>";
+                echo "<table>";
 
-                    // HEADER
-                    echo "<thead><tr>";
-                    $data = fgetcsv($CSVfp, 1000, ",");
-                    echo "<th>".$data[0]."</th><th>".$data[1]."</th><th>".$data[2]."</th><th>".$data[3]."</th>";
-                    echo "</tr></thead>";
+                // HEADER
+                echo "<thead><tr>";
+                $data = fgetcsv($CSVfp, 1000, ",");
+                echo "<th>".$data[0]."</th><th>".$data[1]."</th><th>".$data[2]."</th><th>".$data[3]."</th>";
+                echo "</tr></thead>";
     
-                    while (! feof($CSVfp)) {
+                while (! feof($CSVfp)) {
 
-                        // REST
-                        $data = fgetcsv($CSVfp, 1000, ",");
-
-                        //convert all strings to lower
-                        $search_lower=strtolower($search);            
-
-                        //search the search string in product id or prod name
-                        if (! empty($data)){
-                            $prd_id = $data[0];
-                            $prd_name=$data[1];
-                            //convert the strings to lowercase
-                            $prod_id_lower=strtolower($prd_id);
-                            $prod_name_lower=strtolower($prd_name);
-                            if (str_contains($prod_name_lower, $search_lower) || str_contains($prod_id_lower, $search_lower) ) {
-                                echo "<tr><td>".$data[0]."</td><td>".$data[1]."</td><td>".$data[2]."</td><td>".$data[3]."</td></tr>";
-                            }
-                        }
-
-                    }
-
-                    echo "</table>";
-                }
-            } else {
-
-                $CSVfp = fopen("menuForPhp.csv", "r");
-                if ($CSVfp !== FALSE) {
-
-                    echo "<table>";
-
-                    // HEADER
-                    echo "<thead><tr>";
+                    // REST
                     $data = fgetcsv($CSVfp, 1000, ",");
-                    echo "<th>".$data[0]."</th><th>".$data[1]."</th><th>".$data[2]."</th><th>".$data[3]."</th>";
-                    echo "</tr></thead>";
 
-                    while(! feof($CSVfp)) {
-                        echo "<tr>";
-                        $data = fgetcsv($CSVfp, 1000, ",");
-                        echo "<td>".$data[0]."</td><td>".$data[1]."</td><td>".$data[2]."</td><td>".$data[3]."</td>";
-                        echo "</tr>";
+                    //convert all strings to lower
+                    $search_lower=strtolower($search);            
+
+                    //search the search string in product id, prod name, prod cost or prod price
+                    if (! empty($data)){
+                        $prd_id = $data[0];
+                        $prd_name=$data[1];
+                        $prd_cost=$data[2];
+                        $prd_price=$data[3];
+                        //convert the strings to lowercase
+                        $prod_id_lower=strtolower($prd_id);
+                        $prod_name_lower=strtolower($prd_name);
+                        $prod_price_lower=strtolower(str_replace('$', '', $prd_price));
+                        if (str_contains($prod_name_lower, $search_lower) || str_contains($prod_id_lower, $search_lower) || str_contains($prd_cost, $search) || (strpos($prod_price_lower, $search_lower) !== false)) {
+                            echo "<tr><td>".$data[0]."</td><td>".$data[1]."</td><td>".$data[2]."</td><td>".$data[3]."</td></tr>";
+                        }
                     }
 
-                    echo "</table>";
-                    
                 }
+
+                echo "</table>";
             }
-            
-            fclose($CSVfp);
+        } else {
+
+            $CSVfp = fopen("menuForPhp.csv", "r");
+            if ($CSVfp !== FALSE) {
+
+                echo "<table>";
+
+                // HEADER
+                echo "<thead><tr>";
+                $data = fgetcsv($CSVfp, 1000, ",");
+                echo "<th>".$data[0]."</th><th>".$data[1]."</th><th>".$data[2]."</th><th>".$data[3]."</th>";
+                echo "</tr></thead>";
+
+                while(! feof($CSVfp)) {
+                    echo "<tr>";
+                    $data = fgetcsv($CSVfp, 1000, ",");
+                    echo "<td>".$data[0]."</td><td>".$data[1]."</td><td>".$data[2]."</td><td>".$data[3]."</td>";
+                    echo "</tr>";
+                }
+
+                echo "</table>";
+
+            }
+        }
+        
+        fclose($CSVfp);
     ?>
 
 </body>
